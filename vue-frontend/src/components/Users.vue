5b96a0b69e3b6211c17e0b6d3840519eea9e08bb
Matthew Tin-Tzar Cheung <230502@ac.obigo.com>
2023-05-19 17:48:29 +0900

    added vue

---
+++ vue-frontend/src/components/Users.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class = "container">
+      <h1 class = "text-center"> Users List </h1>
+
+    <table class = "table table-striped">
+        <thead>
+          <tr>
+              <th> User Id </th>
+              <th> User Name </th>
+              <th> User Email </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="user in users" v-bind:key="user.id">
+              <td> {{user.id}}</td>
+              <td> {{user.name}}</td>
+              <td> {{user.email}}</td>
+          </tr>
+        </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+import UserService from '../services/UserService'
+
+export default {
+    name: 'Users-Users',
+    data(){
+        return {
+            users: []
+        }
+    },
+    methods: {
+        getUsers(){
+            UserService.getUsers().then((response) => {
+                this.users = response.data;
+            })
+        }
+    },
+    created() {
+        this.getUsers();
+    }
+}
+
+</script>
